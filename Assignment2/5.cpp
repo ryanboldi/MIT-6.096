e56401a1f6bee84bfd309badd69156f7ebb1e233
Ryan Boldi <Ryan.boldi123@gmail.com>
2021-01-11 21:16:57 +0400

    5 completed

--- Assignment2/5.cpp
+++ Assignment2/5.cpp
@@ -2,13 +2,28 @@
 
 #include <iostream>
 #include <cstdlib>
+#include <cmath>
 
 using namespace std;
 
-int main(){
+double EstimatePi(const int throws){
     srand(time(0));
-    double x = (double)rand()/RAND_MAX;
-    double y = (double)rand()/RAND_MAX;
-    cout << x << "  " << y;
+    int inCirc = 0;
+
+    for (int i = 0; i < throws; i++){
+        double x = (double)rand()/RAND_MAX;
+        double y = (double)rand()/RAND_MAX;
+
+        //don't need to sqrt because r = 1;
+        if (x*x + y*y < 1){
+            inCirc ++;
+        }
+    }
+
+    return (4*(double)inCirc/throws);
+}
+
+int main(){
+    cout << EstimatePi(50000);
     return 0;
 }
