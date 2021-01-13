827f5c82d1874b6f7b479f3b6d97162cfef29c5f
Ryan Boldi <Ryan.boldi123@gmail.com>
2021-01-13 11:58:12 +0400

    assignment 2 completed

--- Assignment2/7.cpp
+++ Assignment2/7.cpp
@@ -13,14 +13,38 @@ int lengthOfString(const char* str){
     return i;
 }
 
+// 7.2
+void swapIntRef(int &a, int &b){
+    int temp = a;
+    a = b;
+    b = temp;
+}
 
+// 7.3
+void swapIntPtr(int *a, int *b){
+    int t = *a;
+    *a = *b;
+    *b = t;
+}
+
+// 7.4
+void swapPtr(int **a, int **b){
+    int *t = *a;
+    *a = *b;
+    *b = t;
+}
 
 int main(void){
-    const char *name;
-    name = "Ryan";
-
-    cout << lengthOfString(name);
-
+//7.5
+    char *oddOrEven = "Never odd or even";
+    char *nthCharPtr = &oddOrEven[5];
+    nthCharPtr = oddOrEven + 3;
+    cout << *nthCharPtr << endl;
+    char **pointerPtr = &nthCharPtr;
+    cout << pointerPtr << endl;
+    cout << **pointerPtr << endl;
+    nthCharPtr = nthCharPtr + 1;
+    cout << nthCharPtr - oddOrEven << endl;
 
     return 0;
 }
